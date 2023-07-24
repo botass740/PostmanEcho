650c0a935e380f3817b378affb51ac7f58aa1d23
Max test <botass740@gmail.com>
2023-07-24 21:49:07 +0300

    Test OK

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -19,6 +19,6 @@ class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Tost"));
+                .body("data", equalTo("Test"));
     }
 }
